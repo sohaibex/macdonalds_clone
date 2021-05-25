13af8c8ded4a45e937e63dfe72f41784c2edbbf6
SOHAIB EL MEDIOUNI <sohaib.elmediouni23@gmail.com>
2021-05-25 03:06:37 +0100

    app bar plus menu

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -7,12 +7,78 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-
+FocusNode myFocusNode = new FocusNode();
 
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.red,
+        elevation: 0.0,
+      ),
+      drawer: Drawer(
+        child: SafeArea(
+
+            child: ListView(
+              children: <Widget>[
+                DrawerHeader(
+                  decoration: BoxDecoration( color: const Color(0xffd70b0b),),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom:80 ),
+                        child: (
+                            Image.asset("images/logo.png")
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                ListTile(
+                  focusNode: myFocusNode,
+                  onTap: ()
+                    {
+                      print("hello");
+                    },
+
+                  autofocus: true,
+                  leading: Icon(Icons.home,color: myFocusNode.hasFocus ? Colors.red : Colors.grey),
+                  title: Text('Accueil',style: TextStyle(fontSize: 16,color: Colors.black),),
+
+                ),
+                ListTile(
+                  leading: Icon(Icons.laptop_mac_rounded,color: Colors.grey,),
+                  title: Text('Fidélité',style: TextStyle(fontSize: 16 ,color: Colors.black),),
+                ),
+                ListTile(
+                  leading: Icon(Icons.fastfood,color: Colors.grey,),
+                  title: Text('Nos Produits',style: TextStyle(fontSize: 16,color: Colors.black),),
+                ),
+                ListTile(
+                  leading: Icon(Icons.map_rounded,color: Colors.grey,),
+                  title: Text('Restaurant',style: TextStyle(fontSize: 16,color: Colors.black),),
+                ),
+                Divider(color: Colors.grey,),
+                ListTile(
+                  leading: Icon(Icons.person,color: Colors.grey,),
+                  title: Text('Mon Compte',style: TextStyle(fontSize: 16,color: Colors.black),),
+                ),
+                ListTile(
+                  leading: Icon(Icons.info_outline_rounded,color: Colors.grey,),
+                  title: Text('A propos de Nous',style: TextStyle(fontSize: 16,color: Colors.black),),
+                ),
+                ListTile(
+                  leading: Icon(Icons.language,color: Colors.grey,),
+                  title: Text('Région et langue',style: TextStyle(fontSize: 16,color: Colors.black),),
+                ),
+
+              ],
+            ),
+
+        ),
+      ),
       body:SingleChildScrollView
         (
       child:Column(
